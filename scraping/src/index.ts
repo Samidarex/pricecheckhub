8b30928b30388ceb6ead641d797a53505c603735
Marcel Prodan <nuta228322@gmail.com>
2023-11-17 20:19:46 +0200

    feat: Created prisma singleton

--- scraping/src/index.ts
+++ scraping/src/index.ts
@@ -1,12 +1,28 @@
 import express from 'express';
 import axios from 'axios';
 import cheerio from 'cheerio';
+import prisma from 'db/prisma';
 
 const app = express();
 const port = 3001;
 
-app.get('/scrape', async (req, res) => {
+app.get('/search/:query', async (req, res) => {
+  const query: unknown = req.params.query;
 
+  try {
+    const newData = await prisma.item.create({
+      data: {
+        name: "test",
+        price: 1,
+        image: "test",
+        link: "test",
+        isAvailable: true
+      }
+    });
+  }
+  catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(port, () => {
