4a156f0b33fdc4089073753cf10f8aa9a449c850
Marcel Prodan <nuta228322@gmail.com>
2023-11-25 19:42:15 +0200

    fix(caching): Removing all keys which value or null or empty

--- caching/cacheService.ts
+++ caching/cacheService.ts
@@ -1,18 +1,58 @@
-import { Redis  } from "ioredis";
+import Redis from 'ioredis';
 
 const redis = new Redis();
 
 export const cacheService = {
     get: async <T>(key: string): Promise<T | null> => {
-      const cachedData = await redis.get(key);
-      return cachedData ? JSON.parse(cachedData) : null;
+        const cachedData = await redis.get(key);
+        return cachedData ? JSON.parse(cachedData) : null;
     },
-    
-    set: async <T>(key: string, data: T, expirationInSeconds: number): Promise<void> => {
-      await redis.set(key, JSON.stringify(data), 'EX', expirationInSeconds);
+
+    set: async <T>(key: string, data: T, expirationInSeconds: number): Promise<boolean> => {
+        // Check if data is not null or undefined
+        if (data !== null && data !== undefined) {
+            // Use SETNX command to set the key only if it does not exist
+            const setIfNotExists = await redis.setnx(key, JSON.stringify(data));
+
+            // If the key did not exist, set the expiration time
+            if (setIfNotExists === 1) {
+                await redis.expire(key, expirationInSeconds);
+                return true;
+            }
+        } else {
+            // If data is null or undefined, remove the key
+            await redis.del(key);
+        }
+
+        return false;
     },
-  
+
     quit: async (): Promise<void> => {
-      await redis.quit();
+        await redis.quit();
     },
-  };
+};
+
+
+export async function getAllDataFromCache() {
+    const allKeys: string[] = await redis.keys('*');
+  
+    const allValues: (string | null)[] = await redis.mget(...allKeys);
+  
+    const cacheData: Record<string, any> = {};
+    allKeys.forEach((key, index) => {
+      if (allValues[index] !== null) {
+        cacheData[key] = JSON.parse(allValues[index]!);
+      }
+    });
+  
+    console.log('All data from Redis cache:', cacheData);
+  
+    await redis.quit();
+}
+
+export async function getMemoryInfo() {
+    const info = await redis.info('memory');
+    console.log('Memory info:', info);
+  
+    await redis.quit();
+}
